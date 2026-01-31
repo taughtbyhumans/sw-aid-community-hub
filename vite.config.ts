785c34969820ed84a7f986299aebb78d7f1dbc1c
Laura <laura@taughtbyhumans.com>
2026-01-31 10:41:01 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: '/sw-aid-community-hub/',
+  base: '/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
